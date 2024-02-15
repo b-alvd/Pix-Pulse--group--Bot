c87731d877ce9a4511d4552d6e75d6c52fcf78ae
Basile ALEVEQUE-DESSOLIN <basilealeveque@gmail.com>
2024-02-15 21:36:10 +0100

    (-) messageCreate (~) ping/ready/package-lock/package (+) loadSlashCommands/interactionCreate

--- Commands/ping.js
+++ Commands/ping.js
@@ -2,6 +2,10 @@ const Discord = require("discord.js")
 
 module.exports = {
     name: "ping",
+    description: "Permet de connaître la latence du bot et de l'API Discord.",
+    permission: "Aucune",
+    dm: true,
+    options: [],
 
     async run(bot, message) {
         await message.reply(`Latence API Discord : \`${bot.ws.ping}ms\`\nLatence Utilisateur : \`${Date.now() - message.createdTimestamp}ms\``)
